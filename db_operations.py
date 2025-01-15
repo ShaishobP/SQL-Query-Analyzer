38ff31ec476d8fe629df0eb1c55a8da19f45c7d3
Shaishob Paul <shaishobpaul6@gmail.com>
2025-01-14 20:49:50 -0500

    Update db_operations.py

--- db_operations.py
+++ db_operations.py
@@ -1,27 +1,23 @@
 import sqlite3
-import time  #Importing time to measure query execution
+import time
 
 def execute_query(query):
     try:
-        # Start measuring time
         start_time = time.time()
 
         connection = sqlite3.connect('sample.db')
         cursor = connection.cursor()
 
-        # Execute the SQL query
         cursor.execute(query)
         connection.commit()
 
-        # Get the results of the query
         result = cursor.fetchall()
 
-        # Measure the execution time
         execution_time = time.time() - start_time
 
-        return result, execution_time  # Return both the results and the execution time
+        return result, execution_time
 
     except sqlite3.Error as e:
-        return f"An error occurred: {e}", 0  # Return 0 for execution time in case of error
+        return f"An error occurred: {e}", 0
     finally:
         connection.close()
